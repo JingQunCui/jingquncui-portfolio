1f9873450230c1b8ee32ed3552111b1dcdeb5414
Jing Cui <jingcui99@gmail.com>
2023-10-23 13:55:45 -0400

    Update

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,58 +1,79 @@
-import AnimatedText from "@/components/AnimatedText";
-import Education from "@/components/Education";
-import Experience from "@/components/Experience";
-import Skills from "@/components/Skills";
-import Layout from "@/components/Layout";
-import Head from "next/head";
-import Image from "next/image";
-import profilePic from "../../public/images/profile/headshot.jpg";
+import React, { useRef } from "react";
+import {motion, useScroll } from "framer-motion"
+import LiIcon from "./LiIcon";
 
-const project = () => {
+const Details = ({ position, company, companyLink, time, work }) => {
+  const ref = useRef(null);
+  return (
+    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+      
+      <LiIcon reference={ref}/>
+      <motion.div
+      initial={{y:50}}
+      whileInView={{y:0}}
+      transition={{duration:0.5, type:"spring"}}
+      >
+        <h3 className="capitalize font-bold text-2xl">
+          {position}&nbsp;
+          <a
+            href={companyLink}
+            target="_blank"
+            className="text-primary dark:text-primaryDark capitalize"
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
+          {time}
+        </span>
+        <p className="font-medium w-full">{work}</p>
+      </motion.div>
+    </li>
+  );
+};
+
+const Projects = () => {
+    const ref = useRef(null);
+    const {scrollYProgress} = useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"]
+        }
+    )
     return (
-      <>
-        <Head>
-        <title>About Me</title>
-          <meta name="description" content="About Me" />
-        </Head>
+      <div className="my-64">
+        <h2 className="font-bold text-8xl mb-32 w-full text-center">
+          Projects
+        </h2>
 
-        <main className="flex w-full flex-col items-center justify-center dark:text-light">
-          <Layout className="pt-16">
-            <AnimatedText
-              text="About Me"
-              className="mb-16 !leading-tight"
-              color="white"
+        <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
+
+        <motion.div style={{scaleY: scrollYProgress}}
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-primaryDark dark:shadow-3xl"/>
+
+        <ul className="w-full flex flex-col items-start justify-center ml-4">
+            <Details
+              position="RCOS: LEGUP"
+              company="Github"
+              companyLink="https://github.com/Bram-Hub/LEGUP"
+              time="Open source project (Java, Swing)"
+              work="Implemented front end features using Java and Swing"
             />
-            <div className="grid w-full grid-cols-8 gap-16">
-              <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
-                <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
-                  Biography
-                </h2>
-                <p className="font-medium">
-                    Ima cum in konain qureshi                  
-                </p>
-              </div>
-  
-              <div className="col-span-5 relative h-max rounded-2xl border-2 border-solid border-darkbg-light p-8 dark:bg-dark dark:border-light">
-                <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
-                    <Image
-                    src={profilePic}
-                    alt="Jesse Cui"
-                    className="w-full h-auto rounded-2xl"
-                    priority
-                    sizes="(max-width: 2000px) 100vw,
-                    (max-width: 2000px) 50vw,
-                    33vw"
-                    />
-              </div>
-            </div>
-  
-            <Skills/>
-            <Experience />
-            <Education />
-          </Layout>
-        </main>
-      </>
-    );
-  };
-  
-  export default project;
+
+            <Details
+              position="RCOS: Excalendar"
+              company="Github"
+              companyLink="https://github.com/GregWith2Gs/Exalendar-React"
+              time="Open source project (Html, Css, Javascript, SQL)"
+              work="Worked on a team responsible for developing a new mobile app
+              feature that allowed users to create and share short-form video
+              content, including designing and implementing a new user interface
+              and developing the backend infrastructure to support the feature."
+            />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
